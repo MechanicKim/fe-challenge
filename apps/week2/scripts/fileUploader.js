e223cac7ebb4ccbb59baae2b4bf465beab7370a8
Jason <jason@Jasonui-MacBookAir.local>
2025-11-04 14:54:40 +0900

    feat: week2 프리뷰, dragover 시각적 피드백

--- apps/week2/scripts/fileUploader.js
+++ apps/week2/scripts/fileUploader.js
@@ -1,15 +1,15 @@
-(() => {
+const FileUploader = ({ uploadURL }) => {
   function DropZone() {
-    const infoText = document.createElement('p');
-    infoText.textContent = '파일을 끌어다 놓거나 클릭하여 선택하세요.';
-    const fileInput = document.createElement('input');
-    fileInput.id = 'file-input';
-    fileInput.type = 'file';
-    fileInput.accept = 'image/*';
-    fileInput.setAttribute('multiple', 'true');
+    const infoText = document.createElement("p");
+    infoText.textContent = "파일을 끌어다 놓거나 클릭하여 선택하세요.";
+    const fileInput = document.createElement("input");
+    fileInput.id = "file-input";
+    fileInput.type = "file";
+    fileInput.accept = "image/*";
+    fileInput.setAttribute("multiple", "true");
 
-    const dropZone = document.createElement('div');
-    dropZone.id = 'drop-zone';
+    const dropZone = document.createElement("div");
+    dropZone.id = "drop-zone";
     dropZone.append(infoText, fileInput);
 
     return {
@@ -19,115 +19,149 @@
   }
 
   function StatusZone() {
-    const statusZone = document.createElement('p');
-    statusZone.id = 'status-zone';
+    const statusZone = document.createElement("p");
+    statusZone.id = "status-zone";
     return statusZone;
   }
 
-  const fileUploader = document.getElementById('file-uploader');
+  function UploadStatusItem(file, timestamp, callback) {
+    const fileName = document.createElement("div");
+    fileName.textContent = file.name;
+    const fileSize = document.createElement("div");
+    fileSize.textContent = `${Math.round(file.size / 1024)}KB`;
+    const uploadStatus = document.createElement("div");
+    uploadStatus.className = "upload-status";
+    const progressBar = document.createElement("div");
+    progressBar.className = "progress-bar";
+    uploadStatus.append(progressBar);
+
+    const infoGroup = document.createElement("div");
+    infoGroup.className = "info-group";
+    infoGroup.append(fileName, fileSize, uploadStatus);
+
+    const preview = document.createElement("img");
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      preview.src = e.target.result;
+      preview.alt = file.name;
+
+      const fileItem = document.createElement("div");
+      fileItem.className = "file-item";
+      fileItem.dataset.file = `${file.name}${timestamp}`;
+      fileItem.append(infoGroup, preview);
+
+      callback(fileItem);
+    };
+    reader.readAsDataURL(file);
+  }
+
+  const fileUploader = document.getElementById("file-uploader");
   const { dropZone, fileInput } = DropZone();
   const statusZone = StatusZone();
   fileUploader.append(dropZone, statusZone);
 
-  function filterFile(items) {
-    return items.filter((item) => item.kind === 'file');
-  }
-
-  function displayStatus(files) {
+  function displayStatus(files, timestamp) {
     for (const file of files) {
-      const fileName = document.createElement('span');
-      fileName.textContent = `${file.name}(${Math.round(file.size / 1024)}KB)`;
-      const uploadStatus = document.createElement('span');
-      uploadStatus.textContent = '대기';
-
-      const fileItem = document.createElement('div');
-      fileItem.className = 'file-item';
-      fileItem.dataset.file = file.name;
-      fileItem.append(fileName, uploadStatus);
-
-      statusZone.append(fileItem);
+      UploadStatusItem(file, timestamp, (fileItem) => statusZone.append(fileItem));
     }
   }
 
-  function updateStatus(fileName, status) {
-    const fileItem = document.querySelector(
-      `.file-item[data-file="${fileName}"]`
+  function updateStatus(dataFile, status) {
+    const fileItemStatus = document.querySelector(
+      `.file-item[data-file="${dataFile}"] .progress-bar`
     );
-    if (fileItem) {
-      fileItem.children[1].textContent = status;
+    if (!fileItemStatus) return;
+    if (status === '성공') {
+      fileItemStatus.classList.add("success");
+    } else {
+      fileItemStatus.classList.add("fail");
     }
   }
 
-  async function uploadFile(file) {
-    const url = 'http://localhost:3000/api/week2/upload';
-
-    const formData = new FormData();
-    formData.append('file', file);
+  async function uploadFile(file, timestamp) {
+    const dataFile = `${file.name}${timestamp}`;
 
     try {
-      const response = await fetch(url, {
-        method: 'POST',
+      const formData = new FormData();
+      formData.append("file", file);
+      const response = await fetch(uploadURL, {
+        method: "POST",
         body: formData,
       });
 
       if (response.ok) {
-        console.log('파일 업로드 성공:', file.name);
-        updateStatus(file.name, '성공');
+        console.log("파일 업로드 성공:", file.name);
+        updateStatus(dataFile, "성공");
       } else {
-        console.error('파일 업로드 실패:', file.name);
-        updateStatus(file.name, '실패');
+        console.error("파일 업로드 실패:", file.name);
+        updateStatus(dataFile, "실패");
       }
     } catch (error) {
-      console.error('업로드 중 에러 발생:', file.name, error);
-      updateStatus(file.name, '에러');
+      console.error("업로드 중 에러 발생:", file.name, error);
+      updateStatus(dataFile, "에러");
     }
   }
 
-  function uploadFiles(files) {
+  function uploadFiles(files, timestamp) {
     for (const file of files) {
-      uploadFile(file);
+      uploadFile(file, timestamp);
     }
   }
 
-  window.addEventListener('dragover', (e) => {
+  function filterFile(items) {
+    return items.filter((item) => item.kind === "file");
+  }
+
+  window.addEventListener("dragover", (e) => {
     const fileItems = filterFile([...e.dataTransfer.items]);
     if (fileItems.length > 0) {
       e.preventDefault();
       if (!dropZone.contains(e.target)) {
-        e.dataTransfer.dropEffect = 'none';
+        dropZone.classList.remove("available");
+        e.dataTransfer.dropEffect = "none";
       }
     }
   });
 
-  window.addEventListener('drop', (e) => {
-    if ([...e.dataTransfer.items].some((item) => item.kind === 'file')) {
+  window.addEventListener("drop", (e) => {
+    if ([...e.dataTransfer.items].some((item) => item.kind === "file")) {
       e.preventDefault();
     }
   });
 
-  dropZone.addEventListener('dragover', (e) => {
+  dropZone.addEventListener("dragover", (e) => {
     const fileItems = filterFile([...e.dataTransfer.items]);
     if (fileItems.length > 0) {
       e.preventDefault();
-      if (fileItems.some((item) => item.type.startsWith('image/'))) {
-        e.dataTransfer.dropEffect = 'copy';
+      if (fileItems.some((item) => item.type.startsWith("image/"))) {
+        dropZone.classList.add("available");
+        e.dataTransfer.dropEffect = "copy";
       } else {
-        e.dataTransfer.dropEffect = 'none';
+        dropZone.classList.remove("available");
+        e.dataTransfer.dropEffect = "none";
       }
     }
   });
 
-  dropZone.addEventListener('drop', (e) => {
+  dropZone.addEventListener("drop", (e) => {
     e.preventDefault();
 
-    const files = [...e.dataTransfer.items]
+    dropZone.classList.remove("available");
+
+    const files = filterFile([...e.dataTransfer.items])
       .map((item) => item.getAsFile())
-      .filter((file) => file.type.startsWith('image/'));
-    displayStatus(files);
-    uploadFiles(files);
+      .filter((file) => file.type.startsWith("image/"));
+    const timestamp = new Date().getTime();
+    displayStatus(files, timestamp);
+    uploadFiles(files, timestamp);
   });
 
-  fileInput.addEventListener('change', (e) => {
-    displayImages(e.target.files);
+  fileInput.addEventListener("change", (e) => {
+    const { files } = e.target;
+    const timestamp = new Date().getTime();
+    displayStatus(e.target.files, timestamp);
+    uploadFiles(files, timestamp);
   });
-})();
+};
+
+FileUploader({ uploadURL: "http://localhost:3000/api/week2/upload" });
