4e27dec688604908e256c285318846d25ec47431
Mingyu Kim <mechanic0406@gmail.com>
2025-11-27 11:33:01 +0900

    feat: week11 심화과제1

--- apps/week11/src/components/AccordionItem.tsx
+++ apps/week11/src/components/AccordionItem.tsx
@@ -1,4 +1,4 @@
-import { useRef, type MouseEventHandler } from "react";
+import { type MouseEventHandler } from "react";
 import styles from "./Accordion.module.css";
 
 interface AccordionItemProps {
@@ -16,14 +16,12 @@ export default function AccordionItem({
   fold,
   toggle,
 }: AccordionItemProps) {
-  const ref = useRef<HTMLDivElement>(null);
-
   return (
     <div className={styles.item}>
-      <h3 id={id} role="button" aria-expanded={!fold} onClick={toggle}>
+      <h3 id={id} role="button" aria-expanded={!fold} tabIndex={0} onClick={toggle}>
         {title}
       </h3>
-      <p ref={ref} role="region" aria-labelledby={id}>
+      <p role="region" aria-labelledby={id}>
         {content}
       </p>
     </div>
